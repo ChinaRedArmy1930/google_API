494a8076d24f3249b348037ae35809b68fce2bea
yangxiaoyu <syyxy@outlook.com>
2019-05-19 20:52:30 +0800

    login 未完成

--- toutiao/get_cookie.py
+++ toutiao/get_cookie.py
@@ -2,7 +2,12 @@ import requests
 import time 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import  slide_verify
+import  slide_verify 
+from io import BytesIO
+
+import  threading
+
+
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -14,42 +19,76 @@ tmp = "/home/yangxiaoyu/tmp";
 browser = webdriver.Chrome(chrome_options=chrome_options);
 
 browser.get("https://sso.toutiao.com/login/?service=https://mp.toutiao.com/sso_confirm/?redirect_url=/")
-#print(browser.page_source)
 
-## 点击登陆按钮
-#login = browser.find_element_by_css_selector('body > div > div.carousel > div.page.page-1 > div > img.i3')
-#login.click()
-#time.sleep(3)
-# 填写手机号
-phone = browser.find_element_by_id('user-mobile')
-phone.send_keys('13530918563')
-# 获取验证码
-browser.find_element_by_id('mobile-code-get').click()
-#此时会弹窗滑动窗口
-window_slide =  browser.find_element_by_id('validate-big')
-small_slide  =  browser.find_element_by_class_name('validate-block')
-#将图片下载到本地
-img = requests.get(window_slide.get_attribute("src"))
-with open(tmp+"/img.jpg", 'wb') as f:
-    f.write(img.content) 
+try:
 
-img = requests.get(small_slide.get_attribute("src"))
-with open(tmp+"/img_small.jpg", 'wb') as f:
-    f.write(img.content) 
+    ## 点击登陆按钮
+    #login = browser.find_element_by_css_selector('body > div > div.carousel > div.page.page-1 > div > img.i3')
+    #login.click()
+    #time.sleep(3)
+    # 填写手机号
+    with open(tmp+"/source.txt", 'wb') as f:
+        f.write(bytes(browser.page_source,encoding='utf8')) 
+    phone = browser.find_element_by_id('user-mobile')
+    phone.send_keys('13530918563')
+    # 获取验证码
+    browser.find_element_by_class_name('get-code').click()
+    #此时会弹窗滑动窗口
 
-#print(window_slide.get_attribute("src"))
+    time.sleep(3)
+    #print(browser.page_source)
+    browser.find_element_by_id('validate-big')
 
-#browser.get_screenshot_as_file("D:/test2/滑动验证/img.jpg")#对整个页面截图
-'''
-verfiy_code_input = input("请输入验证码:")
-# 验证码输入框
-mobile_code = browser.find_element_by_id('mobile-code')
-mobile_code.send_keys(verfiy_code_input)
+    with open(tmp+"/source.txt", 'wb') as f:
+        f.write(bytes(browser.page_source,encoding='utf8')) 
 
-# 登陆
-browser.find_element_by_id('bytedance-SubmitStatic').click()
-time.sleep(5)
-cookies = browser.get_cookies()
-with open('cookies.json', 'w') as f:
-    self.cookies = json.loads(f.write(json.dumps(cookies)))
-'''
+    
+    #将图片下载到本地
+    window_slide =  browser.find_element_by_id('validate-big')
+    small_slide  =  browser.find_element_by_class_name('validate-block')
+    
+    img = requests.get(window_slide.get_attribute("src"))
+    with open(tmp+"/img.jpg", 'wb') as f:
+        f.write(img.content) 
+    
+    img_small = requests.get(small_slide.get_attribute("src"))
+    with open(tmp+"/img_small.jpg", 'wb') as f2:
+        f2.write(img_small.content) 
+
+    
+    img_big = slide_verify.get_image(browser, window_slide);
+
+
+    img_small = slide_verify.get_image(browser, small_slide);
+
+
+
+
+    location = slide_verify.get_distance(img_big, 268,150,img_small);
+    print(location)
+    '''
+    #track_list = get_track(0)
+
+
+
+    #print(window_slide.get_attribute("src"))
+
+    #browser.get_screenshot_as_file("D:/test2/滑动验证/img.jpg")#对整个页面截图
+
+    verfiy_code_input = input("请输入验证码:")
+    # 验证码输入框
+    mobile_code = browser.find_element_by_id('mobile-code')
+    mobile_code.send_keys(verfiy_code_input)
+
+    # 登陆
+    browser.find_element_by_id('bytedance-SubmitStatic').click()
+    time.sleep(5)
+    cookies = browser.get_cookies()
+    with open('cookies.json', 'w') as f:
+        self.cookies = json.loads(f.write(json.dumps(cookies)))
+    '''
+    #e = threading.Event();
+    #e.wait();
+finally:
+    
+    browser.quit();
--- toutiao/slide_verify.py
+++ toutiao/slide_verify.py
@@ -6,80 +6,84 @@ import PIL.Image as image
 from PIL import Image,ImageEnhance
 import time,re, random
 import requests
+from io import BytesIO
+tmp = "/home/yangxiaoyu/tmp";
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 
+#对比两张图片 找到缺口
+def get_distance(image1,image1width,image1height,image2):
+    '''
+      拿到滑动验证码需要移动的距离
+      :param image1:没有缺口的图片对象
+      :param image2:带缺口的图片对象
+      :return:需要移动的距离
+      '''
 
-# 根据位置对图片进行合并还原
-# filename:图片
-# location_list:图片位置
-#内部两个图片处理函数的介绍
-#crop函数带的参数为(起始点的横坐标，起始点的纵坐标，宽度，高度）
-#paste函数的参数为(需要修改的图片，粘贴的起始点的横坐标，粘贴的起始点的纵坐标）
-def get_merge_image(filename,location_list):
-    #打开图片文件
-    im = image.open(filename)
-    #创建新的图片,大小为260*116
-    new_im = image.new("RGB", (260,116))
-    im_list_upper=[]
-    im_list_down=[]
-    # 拷贝图片
+    threshold = 50
+    for i in range(0,image1width):  
+        for j in range(0,image1height):  
+            pixel1 = image1.getpixel((i,j))
+            pixel2 = image2.getpixel((i,j))
+            res_R = abs(pixel1[0]-pixel2[0]) # 计算RGB差
+            res_G = abs(pixel1[1] - pixel2[1])  # 计算RGB差
+            res_B = abs(pixel1[2] - pixel2[2])  # 计算RGB差
+            if res_R > threshold and res_G > threshold and res_B > threshold:
+                return i  # 需要移动的距离
+
+
+
+
+
+def merge_image(image_file,location_list):
+    """
+     拼接图片
+    :param image_file:
+    :param location_list:
+    :return:
+    """
+    im = Image.open(image_file)
+    new_im = Image.new('RGB',(260,116))
+    # 把无序的图片 切成52张小图片
+    im_list_upper = []
+    im_list_down = []
+    # print(location_list)
     for location in location_list:
-        #上面的图片
-        if location["y"]==-58:
-            im_list_upper.append(im.crop((abs(location["x"]),58,abs(location["x"])+10,166)))
-        #下面的图片
-        if location["y"]==0:
-            im_list_down.append(im.crop((abs(location["x"]),0,abs(location["x"])+10,58)))
-    new_im = image.new("RGB", (260,116))
+        # print(location['y'])
+        if location['y'] == -58: # 上半边
+            im_list_upper.append(im.crop((abs(location['x']),58,abs(location['x'])+10,116)))
+        if location['y'] == 0:  # 下半边
+            im_list_down.append(im.crop((abs(location['x']),0,abs(location['x'])+10,58)))
+
     x_offset = 0
-    #黏贴图片
     for im in im_list_upper:
-        new_im.paste(im, (x_offset,0))
+        new_im.paste(im,(x_offset,0))  # 把小图片放到 新的空白图片上
         x_offset += im.size[0]
+
     x_offset = 0
     for im in im_list_down:
-        new_im.paste(im, (x_offset,58))
+        new_im.paste(im,(x_offset,58))
         x_offset += im.size[0]
+    #new_im.show()
     return new_im
 
-#对比RGB值
-def is_similar(image1,image2,x,y):
-    pass
-    #获取指定位置的RGB值
-    pixel1=image1.getpixel((x,y))
-    pixel2=image2.getpixel((x,y))
-    for i in range(0,3):
-        # 如果相差超过50则就认为找到了缺口的位置
-        if abs(pixel1[i]-pixel2[i])>=50:
-            return False
-    return True
+def get_image(driver, img):
+    '''
+    下载无序的图片  然后进行拼接 获得完整的图片
+    :param driver:
+    :param img:
+    :return:
+    '''
+    time.sleep(2)
 
-#计算缺口的位置
-def get_diff_location(image1,image2):
-    i=0
-    # 两张原始图的大小都是相同的260*116
-    # 那就通过两个for循环依次对比每个像素点的RGB值
-    # 如果相差超过50则就认为找到了缺口的位置
-    for i in range(62,260):#有人可能看不懂这个位置为什么要从62开始看最后一张图（图：3）
-        for j in range(0,116):
-            if is_similar(image1,image2,i,j)==False:
-                return  i
+    image_result = requests.get(img.get_attribute("src")).content
 
-#根据缺口的位置模拟x轴移动的轨迹
-def get_track(length):
-    pass
-    list=[]
-    #间隔通过随机范围函数来获得,每次移动一步或者两步
-    x=random.randint(1,3)
-    #生成轨迹并保存到list内
-    while length-x>=5:
-        list.append(x)
-        length=length-x
-        x=random.randint(1,3)
-    #最后五步都是一步步移动
-    for i in range(length):
-        list.append(1)
-    return list
+    image_file = BytesIO(image_result) # 是一张无序的图片
+
+    with open(tmp+"/btyeio.jpg","wb") as f:
+            f.write(image_file.read())
+
+    return image_result
+   
